3dfaa4b77d40f909e71679eee2f61a63ef943db6
Lovish Hamal <lovishhamal123@gmail.com>
2020-08-02 17:54:06 +0545

    modified

--- client/src/Components/Pages/LandingPage/landing-page.js
+++ client/src/Components/Pages/LandingPage/landing-page.js
@@ -27,53 +27,55 @@ export default function Landingpage() {
           </div>
         </div>
         <div className="pt-64 flex items-center justify-center">
-          <div class="bar px-10 flex items-center justify-start py-4 bg-white shadow-lg rounded-lg w-10/12">
-            <RangeDatePicker startDate={date} endDate={tomorrow} />
-            <div class="w-full md:w-1/4 px-3 mb-6 md:mb-0">
-              <div class="relative">
-                <select
-                  class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-4 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="grid-state"
-                >
-                  <option>Single Room</option>
-                  <option>Double Room</option>
-                  <option>Triple Room</option>
-                </select>
-                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                  <svg
-                    class="fill-current h-4 w-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
+          <div class="px-10 py-4 bg-white shadow-lg rounded-lg w-10/12">
+            <div className=" bar flex items-center justify-start">
+              <RangeDatePicker startDate={date} endDate={tomorrow} />
+              <div class="w-full md:w-1/4 px-3 mb-6 md:mb-0">
+                <div class="relative">
+                  <select
+                    class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-4 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="grid-state"
                   >
-                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                  </svg>
+                    <option>Single Room</option>
+                    <option>Double Room</option>
+                    <option>Triple Room</option>
+                  </select>
+                  <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                    <svg
+                      class="fill-current h-4 w-4"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                    </svg>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="w-full md:w-1/4 px-3 mb-6 md:mb-0">
-              <div class="relative">
-                <select
-                  class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-4 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="grid-state"
-                >
-                  <option>Guests</option>
-                  <option>1</option>
-                  <option>2</option>
-                </select>
-                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                  <svg
-                    class="fill-current h-4 w-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
+              <div class="w-full md:w-1/4 px-3 mb-6 md:mb-0">
+                <div class="relative">
+                  <select
+                    class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-4 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="grid-state"
                   >
-                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                  </svg>
+                    <option>Guests</option>
+                    <option>1</option>
+                    <option>2</option>
+                  </select>
+                  <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                    <svg
+                      class="fill-current h-4 w-4"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                    </svg>
+                  </div>
                 </div>
               </div>
+              <button class="bg-blue-500 ml-10 hover:bg-blue-700 text-white text-xs font-bold py-4 px-6 rounded shadow-lg button">
+                Find Rooms
+              </button>
             </div>
-            <button class="bg-blue-500 ml-10 hover:bg-blue-700 text-white text-xs font-bold py-4 px-6 rounded shadow-lg button">
-              Find Rooms
-            </button>
           </div>
         </div>
       </section>
